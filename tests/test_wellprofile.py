50b3be5d5655d389afcd797b73cc2d3c33ef7c01
Kaelin Ellis <ellis.kaelin@gmail.com>
2025-02-08 18:35:58 -0900

    moved generic well profiles into separate .json file.

--- tests/test_wellprofile.py
+++ tests/test_wellprofile.py
@@ -5,6 +5,9 @@ from woffl.geometry.wellprofile import WellProfile
 
 # only works if the command python -m tests.wprof_test is used
 
+# define a well profile, check that the interpolation works
+# how to check the filtration works?
+
 sch_profile = WellProfile.schrader()
 sch_profile.plot_raw()
 sch_profile.plot_filter()
