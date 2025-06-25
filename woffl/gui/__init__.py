3fd1743be12366808f66d11afa3b3f6ba347c985
Scott Pessetto <scott.pessetto@gmail.com>
2025-06-24 17:25:10 -0800

    add back init

---
+++ woffl/gui/__init__.py
@@ -0,0 +1,6 @@
+"""
+GUI package for WOFFL application.
+
+This package contains the graphical user interface components and utilities
+for the WOFFL (Well Optimization and Flow Forecasting Library) application.
+"""
