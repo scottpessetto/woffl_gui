43adc7d0768a6dcb4670e640152d767fad481b75
Scott Pessetto <scott.pessetto@gmail.com>
2025-06-24 17:19:48 -0800

    remove init to try and get databrikcs to work

--- woffl/gui/__init__.py
+++
@@ -1,5 +0,0 @@
-"""GUI Module for WOFFL
-
-This module provides a Streamlit-based graphical user interface for the WOFFL package.
-It allows users to interact with the jetpump functionality through a web interface.
-"""
