36fc4895014efef7e6c250929db0a45e77b36a7b
Scott <scott.pessetto@gmail.com>
2026-08-18 12:53:31 -0800

    purge streamlit.

--- woffl/gui/components/__init__.py
+++
@@ -1,8 +0,0 @@
-"""GUI Components for WOFFL
-
-This module contains reusable UI components for the WOFFL Streamlit GUI.
-"""
-
-from woffl.gui.components.dataframe_display import display_results_table
-
-__all__ = ["display_results_table"]
